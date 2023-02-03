b029052c67fdbbeefbcb447a35e346431054f599
Smithmg7 <115955610+Smithmg7@users.noreply.github.com>
2023-02-03 10:10:29 +0000

    Update Get_Agile_incoming.py

--- Get_Agile_incoming.py
+++ Get_Agile_incoming.py
@@ -1,4 +1,4 @@
-#!/usr/bin/Get_Agile_incoming v2 fix applied value stuck at var 10
+#!/usr/bin/Get_Agile_incomming v2 fix applied value stuck at var 10
 
 # importing the requests library
 import requests
@@ -12,13 +12,14 @@ TARIFF_CODE = "E-1R-" + PRODUCT_CODE + "-B" # change  the "-B" bit to match your
 TARIFF_URL = BASE_URL + "/v1/products/" + PRODUCT_CODE + "/electricity-tariffs/" + TARIFF_CODE
   
 #setting the time
-now = datetime.now()+ timedelta(days = 1)
-DATEFROM = now.strftime("%Y-%m-%d")+ "T00:00Z"
-newdate = now + timedelta(days = 2)
-DATETO = newdate.strftime("%Y-%m-%d")+ "T00:00Z"
+now = datetime.now() + timedelta(days = 0)
+DATEFROM = now.strftime("%Y-%m-%d")+ "T" + now.strftime("%H:%M") +"Z"
+newdate = now + timedelta(hours=25)
+DATETO = newdate.strftime("%Y-%m-%d")+ "T" + newdate.strftime("%H:%M") +"Z"
+
 
 # parameter items given here
-APIKEY = "add your api key here"
+APIKEY = "your api key"
 ACCOUNT = ""
 PERIOD_FROM = DATEFROM
 PERIOD_TO = DATETO
@@ -39,7 +40,15 @@ if r.status_code == 200:
     data = sorted(r.json()['results'], key=lambda k: k['value_inc_vat'])
     tariff_good = '{"results":[ '
     y = 0
+    rates = []
     
+    for value in data:
+         rates.append(value['value_inc_vat'])
+        
+    MinRate = min(rates)
+    MaxRate = max(rates)
+    MeanRate = sum(rates)/len(rates)
+
     
 # parse the json list and extract the rates that meet the criteria
     for item in data:
@@ -57,7 +66,6 @@ if r.status_code == 200:
     json_object = json.loads(CheapRate)
     json_object['results'].sort(key=lambda x: x['from'], reverse=False)
     data = json_object['results']
-    #print(data)
     
 # create final json output
     tariff_new = '{'
@@ -65,13 +73,25 @@ if r.status_code == 200:
     for item in data:
          y = y + 1
          run_num = 'Run_number' +str(y)
-         cost_num = 'cost_number' +str(y) 
+         cost_num = 'cost_number' +str(y)
+         start_num = 'start_time' +str(y)
+         end_num = 'end_time' +str(y)
+         min_rate = 'min_rate' + str(y)
+         max_rate = 'max_rate' + str(y)
+         mean_rate = 'mean_rate' + str(y)
          start_date = str(datetime.fromisoformat(item['from'][:-1]))
+         startdate = datetime.fromisoformat(item['from'][:-1])
+         start_time = startdate.strftime("%H:%M")+ ":00"
+         enddate = startdate + timedelta(minutes=30)
+         end_time = enddate.strftime("%H:%M")+ ":00"
          cost = str(item['Cost'])
-         tariff_new += '"'+ run_num +'": "' + start_date + '",' + '"' + cost_num + '": '+ '"' + cost + '",'
-
+         minrate = str(MinRate)
+         maxrate = str(MaxRate)
+         meanrate = str(MeanRate)
+         tariff_new += '"'+ run_num +'": "' + start_date + '",' + '"' + cost_num + '": '+ '"' + cost + '",'+ '"' + start_num + '": "' + start_time + '","' + end_num +'": "' + end_time + '",' + '"' + min_rate + '": '+ '"' + minrate + '",'+ '"' + max_rate + '": "' + maxrate +'",'+ '"' + mean_rate + '": "' + meanrate + '",'
+    
              
-    print(tariff_new[:-1]+'}')
+    print(tariff_new[:-1]+' }')
 
     r.close()
 
